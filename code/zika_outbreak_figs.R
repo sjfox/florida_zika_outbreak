7f06a55444d89968dce526e16f93ea897a02b0fe
Spencer Fox <spencer.fox85@gmail.com>
2016-10-11 11:51:52 -0500

    update to produce png

--- code/zika_outbreak_figs.R
+++ code/zika_outbreak_figs.R
@@ -3,9 +3,10 @@ library(cowplot)
 library(lubridate)
 
 fl_ts <- read_csv("data/fl_zika_cases.csv")
+
 cum_plot <- ggplot(fl_ts, aes(date, cum_cases)) + geom_line() +
               labs(y="Cumulative Cases", x="Date") + geom_point() + 
               background_grid() 
   
 
-save_plot("figs/cumulative_plot.pdf", plot = cum_plot, base_height = 4, base_aspect_ratio = 1.6)
+save_plot("figs/cumulative_plot.png", plot = cum_plot, base_height = 4, base_aspect_ratio = 1.4)
